41c9138f378d99397d79768ee51585def3110408
Saswata Biswas <saswata.biswas20@gmail.com>
2025-05-30 23:23:22 +0530

    fix

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
